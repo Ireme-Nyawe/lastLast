54e7d9d4d8e18404731b22f66e834b5fc131dd88
Ireme-Nyawe <akimanaj41@gmail.com>
2023-10-20 14:54:15 +0200

    give access to 5173 and remove commented code

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ import routeComment from "./routes/commentRoute";
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000' || 'http://localhost:5173');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
@@ -28,7 +28,6 @@ const options = {
         
         servers :[{
             url: 'https://lastlast.onrender.com',
-            // url: 'http://localhost:4600',
         }],
         security: [
             {
